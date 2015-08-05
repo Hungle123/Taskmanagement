73ad379f88eaa2f0db10b03d498bae7feb7afe82
HungLe <lehung1562@gmail.com>
2015-08-05 11:57:04 +0700

    Add to file user class

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageUser
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        
+        // Constructor that takes no argument
+        public User(){}
+        public void SetUser(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        } 
+
+        public void DisplayUser()
+        {
+            Console.WriteLine("Name: {0} \n Email: {1}", Name, Email);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User u = new User();
+            User u2 = new User();
+            u.SetUser("John","John@america.com");
+            u.DisplayUser();
+            u2.SetUser("Nick", "Nick@clearpathdevelopment.com");
+            u2.DisplayUser();
+            Console.ReadKey();
+        }
+    }
+}
