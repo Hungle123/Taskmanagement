83fbb9ae40f494e26fc4312a51bad4641365ec3b
HungLe <lehung1562@gmail.com>
2015-08-06 09:36:14 +0700

    Task manager
    Continue edited class User and change variable name, method name

--- ManagerUse/ManagerUse/Program.cs
+++ ManagerUse/ManagerUse/Program.cs
@@ -1,39 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ManagerUser
-{
-    public class User
-    {
-        public string Name { get; set; }
-        public string Email { get; set; }
-        
-        // Constructor that takes no argument
-        public User(){}
-        public void SetUser(string name, string email)
-        {
-            Name = name;
-            Email = email;
-        } 
-
-        public void DisplayUser()
-        {
-            Console.WriteLine("Name: {0} \n Email: {1}", Name, Email);
-        }
-    }
+namespace ManagerUse
+{    
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            User u = new User();
-            User u2 = new User();
-            u.SetUser("John","John@america.com");
-            u.DisplayUser();
-            u2.SetUser("Nick", "Nick@clearpathdevelopment.com");
-            u2.DisplayUser();
+            User personOne = new User();
+            User personTwo = new User();
+
+            personOne.SetInformation("John", "John@america.com");
+            personOne.Display();
+
+            personTwo.SetInformation("Nick", "Nick@clearpathdevelopment.com");
+            personTwo.Display();
+
             Console.ReadKey();
         }
     }
---
+++ ManagerUse/ManagerUse/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ManagerUse
+{
+    class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        // Constructor that takes no argument
+        public void SetInformation(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        //This is method display infomation for user
+        public void Display()
+        {
+            Console.WriteLine("Name for user: {0}", Name);
+            Console.WriteLine("Email for user: {0}", Email);
+        }
+    }
+}
