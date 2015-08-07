2032bed7a265999c0e17160b150d2e244e32b4d4
HungLe <lehung1562@gmail.com>
2015-08-07 09:21:36 +0700

    Create class Read list of User from a text file user.txt

--- ManagerUse/ManagerUse/Program.cs
+++ ManagerUse/ManagerUse/Program.cs
@@ -6,15 +6,22 @@ namespace ManagerUse
     {
         static void Main()
         {
-
-            User personOne = new User();
-            User personTwo = new User();
+            
+            /* User personOne = new User();
+               User personTwo = new User();
 
             personOne.SetInformation("John", "John@america.com");
             personOne.Display();
 
             personTwo.SetInformation("Nick", "Nick@clearpathdevelopment.com");
-            personTwo.Display();
+            personTwo.Display(); 
+             * */
+
+            /*
+             * ReadUser content = new ReadUser();
+            content.ReadText();
+            content.Display(); 
+             */
 
             Console.ReadKey();
         }
---
+++ ManagerUse/ManagerUse/ReadUser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace ManagerUse
+{  
+    class ReadUser
+    {
+        public string[] infoUsers;
+
+        // This id method read list of user from a text file
+        public void ReadText()
+        {
+            infoUsers = File.ReadAllLines(@"C:\Users\dn00001\Documents\GitHub\Taskmanagement\user.txt");
+        }
+
+        // This is a method display infomation of user froma text file
+        public void Display()
+        {
+           Console.WriteLine("Content a text file: ");
+            foreach (var infoUser in infoUsers)
+            {
+                  Console.WriteLine("\t" +infoUser);
+            }
+        }
+    }
+}
